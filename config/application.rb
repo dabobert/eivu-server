86d10ffd8d7b554526b34062ac1e41bd4cd5004c
Robert Jenkins <bobert@gmail.com>
2019-08-27 22:02:22 -0400

    rspec is the default genrator for tests, and created a traffic control initalizer

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,13 @@ module Eivu
       Devise::SessionsController.skip_before_action :authenticate_user!
     end
 
+    # ActiveJob configuration
+    config.active_job.queue_adapter = :sidekiq
+
+    config.generators do |g|
+      g.test_framework :rspec
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
